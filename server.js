ef70e59167a3bce0edf7b44266af26c151c785ea
LakshayySharma <44471078+LakshayySharma@users.noreply.github.com>
2023-01-29 10:55:45 +0530

    initial setup

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 
-app.listen(8080, (req, res) => {
+app.get("/", (req, res) => {
+  res.json({
+    msg: "setup done",
+  });
+});
+
+app.listen(process.env.PORT, (req, res) => {
   console.log(`server started`);
 });
